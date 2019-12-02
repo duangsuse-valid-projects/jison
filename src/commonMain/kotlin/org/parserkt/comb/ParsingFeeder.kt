8cfe23139ed95558581d53ade2a1ff43422c48b2
duangsuse <fedora-opensuse@outlook.com>
2019-12-02 17:39:43 +0800

    Fix for ParsingFeeder: line numbers

--- src/commonMain/kotlin/org/parserkt/comb/ParsingFeeder.kt
+++ src/commonMain/kotlin/org/parserkt/comb/ParsingFeeder.kt
@@ -11,57 +11,77 @@ data class SourceLocation(var file: String?, var line: Cnt = 1, var column: Cnt 
   override fun toString(): String = "$tag #$position"
 }
 
-val DEFAULT_VIEWPORT = Triple(-10, 10, "")
-/** [ParserError.briefView] take size (actual size may lesser): _(l, r, join)_ */
-var briefViewport = { _: SliceFeeder<*>? -> DEFAULT_VIEWPORT }
+/** Default [briefViewport] is __(-10, 10]__ join by `""` */
+val DEFAULT_VIEWPORT = Triple(-10, 10.dec(), "")
+/** [ParserError.briefView] take offset (actual size may lesser): _(l, r, join)_ (inclusive) */
+var briefViewport = { _: Feeder<*>? -> DEFAULT_VIEWPORT }
 
 /**
  * [Feeder] with source location decoration [quake]
  * + [ScannerOpts] is not supported by this utility, since [ScannerOpts.dropWhileIn] is uncounted
  */
 open class ParsingFeeder<out T>(protected open val inner: Feeder<T>,
-    private val srcLoc: SourceLocation): Feeder<T> by inner, MarkReset {
+    protected val srcLoc: SourceLocation): Feeder<T> by inner, MarkReset {
   constructor(slice: Slice<T>, file: String? = null): this(SliceFeeder(slice), SourceLocation(file))
   constructor(stream: Iterator<T>, file: String? = null): this(StreamFeeder(stream), SourceLocation(file))
 
-  override fun consume(): T {
-    val took = inner.consume()
-    updateSrcLoc(took)
-    return took
-  }
+  override fun consume(): T = inner.consume().also(::updateSrcLoc)
+  override fun quake(error: Exception): Nothing = throw ParserError("@$srcLoc: ${error.message}", briefView())
   protected open fun updateSrcLoc(item: Any?) {
     ++srcLoc.position; ++srcLoc.column
     when (item) {
       '\n' -> { ++srcLoc.line; srcLoc.column = 1 }
       '\r' -> {
         ++srcLoc.line; srcLoc.column = 1
-        if (inner.peek == '\n') inner.consume()
-      }
+        if (inner.peek == '\n') inner.consume() }
     }
   }
-  override fun quake(error: Exception): Nothing = throw ParserError("@$srcLoc: ${error.message}", briefView())
+
   protected open fun briefView(): String? {
-    val cfg = briefViewport(inner as? SliceFeeder<T>)
+    val cfg = briefViewport(inner)
     return when (val innerFeed = inner) {
       is SliceFeeder<T> -> innerFeed[Pair(cfg.first, cfg.second)]
-        .stream().toList().joinToString(cfg.third) //NOTE: data pointer will not mutate
-      is StreamFeeder<T>, is StreamFeederLookahead1<T> -> innerFeed.takeItemN(cfg.second).joinToString(cfg.third)
+        .stream().asSequence().joinToString(cfg.third) //NOTE: data pointer will not mutate
+      is StreamFeeder<T>, is StreamFeederLookahead1<T> -> innerFeed
+        .takeItemN(cfg.second).joinToString(cfg.third)
       is BulkFeeder<T> -> innerFeed.take(cfg.second).consume()
-        .stream().toList().joinToString(cfg.third)
+        .stream().asSequence().joinToString(cfg.third)
       else -> null
     }
   }
 
   override fun mark() { (inner as? MarkReset)?.mark() }
   override fun reset() { (inner as? MarkReset)?.reset() }
+  override fun unmark() { (inner as? MarkReset)?.unmark() }
 }
 
 class BulkParsingFeeder<out T>(override val inner: BulkFeeder<T>, srcLoc: SourceLocation):
   ParsingFeeder<T>(inner, srcLoc), BulkFeeder<T> {
   constructor(inner: BulkFeeder<T>, file: String?): this(inner, SourceLocation(file))
 
-  override fun take(n: Cnt): BulkFeeder.Viewport<T>
-    = inner.take(n).also { it.stream().forEach(::updateSrcLoc) }
+  private fun updateSrcLoc(items: Slice<Any?>) {
+    srcLoc.position += items.size.dec()
+    for (i in items.indices) when (items[i]) {
+      '\n' -> { ++srcLoc.line }
+      '\r' -> {
+        if (i.inc() == items.size
+          ||items[i.inc()] != '\n') ++srcLoc.line
+      }
+    }
+    val oldColumn = srcLoc.column
+    findMark@ for ((i, c) in items.indices.reversed().withIndex()) when (items[c]) {
+      '\r' -> { srcLoc.column = items.size-i; break@findMark }
+      '\n' -> { srcLoc.column = items.size-i
+        if (items[c.dec()] == '\r') --srcLoc.column
+        break@findMark }
+    }
+    if (srcLoc.column == oldColumn) srcLoc.column += items.size.dec() // in-line
+  }
+
+  override fun take(n: Cnt): BulkFeeder.Viewport<T> = inner.take(n).let { view ->
+    object: BulkFeeder.Viewport<T> by view {
+      override fun consume(): Slice<T> = view.consume().also(this@BulkParsingFeeder::updateSrcLoc)
+    } }
 }
 
 fun parsingFeeder(str: CharSequence, file: String? = null): ParsingFeeder<Char>
--- src/commonTest/kotlin/comb/ParsingFeederTests.kt
+++ src/commonTest/kotlin/comb/ParsingFeederTests.kt
@@ -1,20 +1,23 @@
 package comb
 
 import assertMessageEquals
+import org.parserkt.Feeder
 import org.parserkt.comb.ParserError
 import org.parserkt.comb.ParsingFeeder
 import org.parserkt.comb.bulkParsingFeeder
+import org.parserkt.comb.parsingFeeder
 import org.parserkt.dropWhile
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class ParsingFeederTests {
   object NoError: Exception("nope")
-  private val pf = bulkParsingFeeder(str = """
+  private val pf = parsingFeeder(str = """
       hello,
       cruel
       world.
-    """.trimIndent() + "\r\nCRLF", file = "test.txt")
+    """.trimIndent() + "\r\nCRLF\r.", file = "test.txt")
   @Test fun sourceLocated() {
     fun assertMessage(expected: String): Unit = assertFailMessageEquals(expected, pf)
     assertMessage("parser fail@test.txt:1:1 #0: nope")
@@ -29,8 +32,43 @@ class ParsingFeederTests {
 
     pf.dropWhile { it != 'C' }
     assertMessage("parser fail@test.txt:4:1 #20: nope")
+
+    pf.dropWhile { it != '.' }
+    assertMessage("parser fail@test.txt:5:1 #25: nope")
+  }
+
+  private val bpf = bulkParsingFeeder(str = """
+    One day, a cool boy
+      living in a pretty house
+     fall in love with
+       an ugly frog.
+  """.trimIndent()+"\r"+"""
+    The frog says:
+  """.trimIndent()+"\r\n"+"""
+    "woof! woof!!!"
+    And many years later, they lived happily together.
+  """.trimIndent())
+  @Test fun bulkReading() {
+    fun assertMessage(expected: String): Unit = assertFailMessageEquals(expected, bpf)
+    assertMessage("parser fail@<anon>:1:1 #0: nope")
+    bpf.take(10).ignore()
+    assertMessage("parser fail@<anon>:1:1 #0: nope")
+    bpf.take(10).consume()
+    assertMessage("parser fail@<anon>:1:1 #0: nope")
+  }
+  @Test fun brief() {
+    val view = briefView(pf)
+    assertEquals(10, view?.length)
+    assertEquals("hello,\ncru", view)
+
+    bpf.take(10).consume()
+    val view1 = briefView(bpf)
+    assertEquals(20, view1?.length)
+    assertEquals("One day, a cool boy\n", view1)
   }
 
   private fun assertFailMessageEquals(expected: String, pf: ParsingFeeder<*>)
     = assertMessageEquals<ParserError>(expected) { pf.quake(NoError) }
+  private fun briefView(feed: Feeder<*>): String?
+    = assertFailsWith<ParserError> { feed.quake(NoError) }.briefView
 }
